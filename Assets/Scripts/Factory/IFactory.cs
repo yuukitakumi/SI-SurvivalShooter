9880a3f932aaaca9c1599640ba55cc439424d581
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2021-09-29 09:46:50 +0700

    1.0
    Project

---
+++ Assets/Scripts/Factory/IFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Start is called before the first frame update
+public interface IFactory
+{
+    GameObject FactoryMethod(int tag);
+}
+
---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    public Text warningText;
+    public PlayerHealth playerHealth;       
+    public float restartDelay = 5f;            
+
+
+    Animator anim;                          
+    float restartTimer;
+    public bool isDead = false;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            if (!isDead)
+            {
+                isDead = true;
+                anim.SetTrigger("GameOver");
+            }                        
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+    public void ShowWarning(float enemyDistance)
+    {
+        warningText.text = string.Format("! {0} m", Mathf.RoundToInt(enemyDistance));
+        anim.SetTrigger("Warning");
+    }
+}
